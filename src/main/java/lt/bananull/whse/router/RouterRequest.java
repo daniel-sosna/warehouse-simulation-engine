8fd9ec56eedcd3915dca2fb88ddd9b9b5c406a30
Laimis Papinigis <laimis.papinigis@mif.stud.vu.lt>
2026-03-14 22:20:41 +0200

    Add  @JsonProperty and fix now time to simulation now time.

--- src/main/java/lt/bananull/whse/router/RouterRequest.java
+++ src/main/java/lt/bananull/whse/router/RouterRequest.java
@@ -1,59 +1,62 @@
 package lt.bananull.whse.router;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lt.bananull.whse.dto.dataset.BinDto;
 import lt.bananull.whse.dto.dataset.GridDto;
+import lt.bananull.whse.dto.dataset.PortDto;
 import lt.bananull.whse.dto.dataset.ShiftDto;
 import lt.bananull.whse.dto.dataset.ShipmentDto;
 import lt.bananull.whse.load.SimulationState;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
+import java.time.*;
 import java.util.Comparator;
 import java.util.List;
 
-public record RouterRequest(State state) {
+public record RouterRequest(
+        @JsonProperty("state") State state
+) {
 
-    public static RouterRequest from(SimulationState state) {
-        // TODO should be our simulation time
-        Instant now = Instant.now().truncatedTo(ChronoUnit.SECONDS);
-
+    public static RouterRequest from(SimulationState simulationState) {
         ZoneId zone = ZoneId.of("UTC");
-        LocalDate simulationDate = resolveSimulationDateFromShipments(state, zone);
-        List<RouterGrid> grids = mapGrids(state.grids(), simulationDate, zone);
+
+        LocalDate simulationDate = resolveSimulationDateFromShipments(simulationState, zone);
+
+        Instant simulationNow = resolveSimulationNow(simulationState, simulationDate, zone);
+
+        List<RouterGrid> grids = mapGrids(simulationState.grids(), simulationDate, zone);
+
         State routerState = new State(
-                now,
-                state.shipments(),
-                state.bins(),
+                simulationNow,
+                simulationState.shipments(),
+                simulationState.bins(),
                 grids
         );
 
         return new RouterRequest(routerState);
     }
 
-    public record State(
-            Instant now,
-            List<ShipmentDto> shipments_backlog,
-            List<BinDto> stock_bins,
-            List<RouterGrid> grids
+
+    private record State(
+            @JsonProperty("now") Instant now,
+            @JsonProperty("shipments_backlog") List<ShipmentDto> shipmentsBacklog,
+            @JsonProperty("stock_bins") List<BinDto> stockBins,
+            @JsonProperty("grids") List<RouterGrid> grids
     ) {}
 
-    public record RouterGrid(
-            String id,
-            List<RouterShift> shifts
+    private record RouterGrid(
+            @JsonProperty("id") String id,
+            @JsonProperty("shifts") List<RouterShift> shifts
     ) {}
 
-    public record RouterShift(
-            Instant start_at,
-            Instant end_at,
-            List<RouterPortConfig> port_config
+    private record RouterShift(
+            @JsonProperty("start_at") Instant startAt,
+            @JsonProperty("end_at") Instant endAt,
+            @JsonProperty("port_config") List<RouterPortConfig> portConfig
     ) {}
 
-    public record RouterPortConfig(
-            String port_id,
-            List<String> handling_flags
+    private record RouterPortConfig(
+            @JsonProperty("port_id") String portId,
+            @JsonProperty("handling_flags") List<String> handlingFlags
     ) {}
 
     private static LocalDate resolveSimulationDateFromShipments(SimulationState state, ZoneId zone) {
@@ -64,26 +67,54 @@ public record RouterRequest(State state) {
                 .orElseGet(() -> LocalDate.now(zone));
     }
 
-    private static List<RouterGrid> mapGrids(List<GridDto> dtos, LocalDate simulationDate, ZoneId zone) {
+    private static Instant resolveSimulationNow(SimulationState state,
+                                                LocalDate simulationDate,
+                                                ZoneId zone) {
+        return state.grids().stream()
+                .flatMap(grid -> grid.shifts().stream())
+                .map(ShiftDto::start)
+                .min(Comparator.naturalOrder())
+                .map(firstShiftStart ->
+                        firstShiftStart
+                                .atDate(simulationDate)
+                                .atZone(zone)
+                                .toInstant()
+                )
+                .orElseGet(() ->
+                        LocalTime.MIDNIGHT
+                                .atDate(simulationDate)
+                                .atZone(zone)
+                                .toInstant()
+                );
+    }
+
+    private static List<RouterGrid> mapGrids(List<GridDto> dtos,
+                                             LocalDate simulationDate,
+                                             ZoneId zone) {
         return dtos.stream()
                 .map(dto -> mapGrid(dto, simulationDate, zone))
                 .toList();
     }
 
-    private static RouterGrid mapGrid(GridDto dto, LocalDate simulationDate, ZoneId zone) {
-        List<RouterShift> shifts = dto.shifts().stream()
+    private static RouterGrid mapGrid(GridDto dto,
+                                      LocalDate simulationDate,
+                                      ZoneId zone) {
+        List<RouterShift> shifts = dto.shifts() == null ? List.of()
+                : dto.shifts().stream()
                 .map(shiftDto -> mapShift(shiftDto, simulationDate, zone))
                 .toList();
 
         return new RouterGrid(dto.id(), shifts);
     }
 
-    private static RouterShift mapShift(ShiftDto dto, LocalDate simulationDate, ZoneId zone) {
+    private static RouterShift mapShift(ShiftDto dto,
+                                        LocalDate simulationDate,
+                                        ZoneId zone) {
         List<RouterPortConfig> ports =
-                dto.portConfig() == null ? List.of() :
-                        dto.portConfig().stream()
-                                .map(portDto -> new RouterPortConfig(portDto.id(), portDto.handlingFlags()))
-                                .toList();
+                dto.portConfig() == null ? List.of()
+                        : dto.portConfig().stream()
+                        .map(RouterRequest::mapPort)
+                        .toList();
 
         Instant start = dto.start()
                 .atDate(simulationDate)
@@ -96,5 +127,12 @@ public record RouterRequest(State state) {
                 .toInstant();
 
         return new RouterShift(start, end, ports);
+    }
+
+    private static RouterPortConfig mapPort(PortDto dto) {
+        return new RouterPortConfig(
+                dto.id(),
+                dto.handlingFlags()
+        );
     }
 }
