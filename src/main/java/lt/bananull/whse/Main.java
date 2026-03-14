d726424382d0e9521e5281f310e69cfde7f245d8
stitas <81618250+stitas@users.noreply.github.com>
2026-03-14 17:13:42 +0200

    update main method

--- src/main/java/lt/bananull/whse/Main.java
+++ src/main/java/lt/bananull/whse/Main.java
@@ -1,17 +1,31 @@
 package lt.bananull.whse;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import lt.bananull.whse.router.RouterClient;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static String dataDir = "./data";
+    private static String routerCmd = "./build/router";
+    private static String eventLogFile = "./simulation.log";
+
+    public static void main(String[] args) {
+        collectArgs(args);
+
+        // TODO 1. read files from dataDir
+
+        // Create router client
+        RouterClient routerClient = new RouterClient(routerCmd);
+
+        // TODO 2. main while loop
+    }
+
+    private static void collectArgs(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "--dataDir" -> dataDir = args[++i];
+                case "--router" -> routerCmd = args[++i];
+                case "--eventLogFile" -> eventLogFile = args[++i];
+                default -> System.err.println("Unknown argument: " + args[i]);
+            }
         }
     }
 }
