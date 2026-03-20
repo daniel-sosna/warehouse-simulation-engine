770c05f782fde3968b4f9c12026bbed2cf0d90a5
Laimis Papinigis <laimis.papinigis@mif.stud.vu.lt>
2026-03-20 22:05:30 +0200

    Fix logsorter error

--- src/main/java/lt/bananull/whse/Main.java
+++ src/main/java/lt/bananull/whse/Main.java
@@ -1,5 +1,6 @@
 package lt.bananull.whse;
 
+import ch.qos.logback.classic.LoggerContext;
 import lt.bananull.whse.config.AppConfig;
 import lt.bananull.whse.simulator.Simulator;
 import lt.bananull.whse.simulator.SimulationParameters;
@@ -26,6 +27,10 @@ public class Main {
         // Main loop
         Simulator simulator = new Simulator(routerClient, state, parameters);
         simulator.run();
+
+        if (LoggerFactory.getILoggerFactory() instanceof LoggerContext loggerContext) {
+            loggerContext.stop();
+        }
 
         try {
             LogFileSorter.sortSimulationLogBySimTime(config.eventLogFile());
