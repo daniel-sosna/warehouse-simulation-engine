a0681abc30cea49302f6a16ae0984af457eb895b
Rokas <rdidrikas@gmail.com>
2026-03-21 12:28:21 +0200

    move the shift resolver to a service class

--- src/main/java/lt/bananull/whse/event/events/PortOpensEvent.java
+++ src/main/java/lt/bananull/whse/event/events/PortOpensEvent.java
@@ -3,6 +3,7 @@ package lt.bananull.whse.event.events;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import lt.bananull.whse.event.Event;
+import lt.bananull.whse.service.PortShiftService;
 import lt.bananull.whse.simulator.Simulator;
 import lt.bananull.whse.simulator.entity.Grid;
 import lt.bananull.whse.simulator.entity.Port;
@@ -29,7 +30,7 @@ public class PortOpensEvent extends Event {
     @Override
     public void execute(Simulator simulator) {
         Instant now = simulator.getNow();
-        Shift shift = findCurrentOrNextShift(simulator.getState().getGrid(gridId), portId, now);
+        Shift shift = PortShiftService.findCurrentOrNextShift(simulator.getState().getGrid(gridId), portId, now);
         if (shift == null) return; // no more shifts left
         if (now.isBefore(shift.getStartAt())) {
             simulator.enqueueEvent(new PortOpensEvent(DateTimeResolver.resolveSimTimeFromTimestamp(shift.getStartAt()
@@ -41,14 +42,6 @@ public class PortOpensEvent extends Event {
         Event next = new PortClosesEvent(DateTimeResolver.resolveSimTimeFromTimestamp(shift.getEndAt(),
             simulator.getParameters().simulationStartTime()), shift.getEndAt(), gridId, portId);
         simulator.enqueueEvent(next);
-    }
-
-    private static Shift findCurrentOrNextShift(Grid grid, String portId, Instant now) {
-        return grid.getShifts().stream()
-            .filter(s -> s.getPortIds().contains(portId))
-            .filter(s -> !s.getEndAt().isBefore(now)) // endAt >= now
-            .min(Comparator.comparing(Shift::getStartAt))
-            .orElse(null);
     }
 
     @Override
--- src/main/java/lt/bananull/whse/service/PortShiftService.java
+++ src/main/java/lt/bananull/whse/service/PortShiftService.java
@@ -1,5 +1,19 @@
 package lt.bananull.whse.service;
 
+import lt.bananull.whse.simulator.entity.Grid;
+import lt.bananull.whse.simulator.entity.Shift;
+
+import java.time.Instant;
+import java.util.Comparator;
+
 public class PortShiftService {
+
+    public static Shift findCurrentOrNextShift(Grid grid, String portId, Instant now) {
+        return grid.getShifts().stream()
+            .filter(s -> s.getPortIds().contains(portId))
+            .filter(s -> !s.getEndAt().isBefore(now)) // endAt >= now
+            .min(Comparator.comparing(Shift::getStartAt))
+            .orElse(null);
+    }
 
 }
