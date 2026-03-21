35d7b284c052cef3711b3fa919beb700e8e01da1
Rokas <rdidrikas@gmail.com>
2026-03-21 13:28:40 +0200

    remove todo because it is done

--- src/main/java/lt/bananull/whse/simulator/entity/Port.java
+++ src/main/java/lt/bananull/whse/simulator/entity/Port.java
@@ -30,7 +30,7 @@ public class Port {
         this.id = id;
         this.handlingFlags = Set.copyOf(handlingFlags);
         this.queueCapacity = queueCapacity;
-        this.status = PortStatus.CLOSED; // TODO: for now it will be open to test how the queues work
+        this.status = PortStatus.CLOSED;
         this.shipmentQueue = new ArrayDeque<>(queueCapacity);
     }
 
