18bfe211746513f6f32c9d00055b5f6eb1ee543d
stitas <81618250+stitas@users.noreply.github.com>
2026-03-21 19:11:56 +0200

    remove random spaces

--- src/main/java/lt/bananull/whse/event/events/PortClosesEvent.java
+++ src/main/java/lt/bananull/whse/event/events/PortClosesEvent.java
@@ -17,7 +17,7 @@ public class PortClosesEvent extends Event {
     private final Instant endsAt;
     private final Shift.BreakOccurrence portBreak;
 
-    public  PortClosesEvent(long simTime, Instant endsAt, String gridId, String portId,
+    public PortClosesEvent(long simTime, Instant endsAt, String gridId, String portId,
                             Shift.BreakOccurrence portBreak){
         super(simTime);
         this.endsAt = endsAt;
@@ -49,7 +49,7 @@ public class PortClosesEvent extends Event {
             nextShift.getStartAt(),
             simulator.getParameters().simulationStartTime()
         );
-        Event next =  new PortOpensEvent(openAt, gridId, portId, false);
+        Event next = new PortOpensEvent(openAt, gridId, portId, false);
         simulator.enqueueEvent(next);
     }
 
